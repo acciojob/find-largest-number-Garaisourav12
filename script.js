86925a95a75e19e2d8e69b1d728824fc58c1cbaa
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2024-06-10 23:16:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function findLargest(a, b, c) {
   //your code here
+	let max = num1>num2 ? (num1>num3 ? num1 : num3) : (num2>num3 ? num2 : num3);
+	return max;
 }
 
 const num1 = parseInt(prompt("Enter First Number."));
